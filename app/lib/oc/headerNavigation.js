85d8b75740e06c676d201c21bf08abe3940e2ec6
kylejpatrick <kyle_patrick@coverdalegroup.com>
2017-05-23 22:31:08 -0400

    First pass at ngFader app; remove messaging header nav

--- app/lib/oc/headerNavigation.js
+++ app/lib/oc/headerNavigation.js
@@ -14,9 +14,6 @@ function headernavigation() {
         return [
             '<section class="header-navigation navbar-fixed-top">',
             '<div class="row">',
-            '<div class="col-xs-12 col-sm-6 pull-left" style="padding-top: 5px; font-weight:normal">',
-            'Introducing the new tech category! 10% off with coupon GTETECH through April 30th!',
-            '</div>',
             '<div class="col-xs-12 col-sm-6 pull-right">',
             '<ul>',
             '<li><a href="admin">My Account</a></li>',
